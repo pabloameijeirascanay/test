c87d4f68007e92b823c5e5f05d59dbc84455ff8b
pabloameijeirascanay <pabloam@microsoft.com>
2023-04-08 08:33:49 +0200

    update

--- artifacts/download.sh
+++ artifacts/download.sh
@@ -20,7 +20,8 @@ echo $AKS_NAME:$8 | awk '{print substr($1,2); }' >> vars.sh
 echo $AKV_NAME:$9 | awk '{print substr($1,2); }' >> vars.sh
 echo $CERT_NAME:${10} | awk '{print substr($1,2); }' >> vars.sh
 echo $DNS_PRIVATE_ZONE_RESOURCE_GROUP_NAME:${11} | awk '{print substr($1,2); }' >> vars.sh
-echo $TEMPLATE_BASE_URL:${11} | awk '{print substr($1,2); }' >> vars.sh 
+echo $TEMPLATE_BASE_URL:${11} | awk '{print substr($1,2); }' >> vars.sh
+echo $AKV_RESOURCE_GROUP_NAME:${11} | awk '{print substr($1,2); }' >> vars.sh 
 
 sed -i '2s/^/export ADMIN_USER_NAME=/' vars.sh
 sed -i '3s/^/export SPN_CLIENT_ID=/' vars.sh
@@ -34,6 +35,7 @@ sed -i '10s/^/export AKV_NAME=/' vars.sh
 sed -i '11s/^/export CERT_NAME=/' vars.sh
 sed -i '12s/^/export DNS_PRIVATE_ZONE_RESOURCE_GROUP_NAME=/' vars.sh
 sed -i '13s/^/export TEMPLATE_BASE_URL=/' vars.sh
+sed -i '13s/^/export AKV_RESOURCE_GROUP_NAME=/' vars.sh
 
 chmod +x vars.sh 
 . ./vars.sh
