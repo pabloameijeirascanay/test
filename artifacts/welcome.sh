0ec7afce6cf1862b93d33f2ecdc16530f4e2334f
pabloameijeirascanay <pabloam@microsoft.com>
2023-04-08 22:33:21 +0200

    update

--- artifacts/welcome.sh
+++ artifacts/welcome.sh
@@ -2,7 +2,7 @@ tput setaf 1;echo "-------------------------------------------------------------
 echo ""
 tput setaf 6;echo "Welcome to the Azure Firewall TLS inspection scenario!"
 echo ""
-tput setaf 6;echo "* To check the configuration deployment log, use the 'cat jumpstart_logs/installKubeadm.log' command."
+tput setaf 6;echo "* To check the custom script download.sh log, use the 'cat download.log' command."
 echo ""
 tput setaf 6;echo "* To work with the AKS cluster, use the 'kubectl <command>'. For example: kubectl get nodes"
 echo ""
